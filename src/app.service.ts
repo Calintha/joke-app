41170debedb8b046b4cfd17119b6b2b0ae1dd89a
calintha <cal.intha77@gmail.com>
2022-09-15 09:58:13 +0200

    Add flags

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 
   async getJoke(): Promise<Joke> {
     const response = await this.httpService.get(
-      'https://v2.jokeapi.dev/joke/Any',
+      'https://v2.jokeapi.dev/joke/Programing?blacklistFlags=nsfw,religious,political,racist,sexist,explicit,dark',
       {
         headers: {
           accept: 'application/json',
